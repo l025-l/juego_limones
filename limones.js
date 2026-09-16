38f89520bb9e1b392dee5b23df85dc2ee38276b6
l025-l <cristianpegatodo22@gmail.com>
2026-09-16 18:12:30 -0500

    mover a la izquierda

--- limones.js
+++ limones.js
@@ -5,6 +5,8 @@ const ALTURA_SUELO=40
 const ALTUTA_PERSONAJE=60
 const ANCHO_PERSONAJE=40
 
+let personajeX=canvas.width/2;
+
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
@@ -17,5 +19,20 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTUTA_PERSONAJE),ANCHO_PERSONAJE,ALTUTA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTUTA_PERSONAJE),ANCHO_PERSONAJE,ALTUTA_PERSONAJE);
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
+    limpiarCanvas();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
