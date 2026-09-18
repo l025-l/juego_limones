6436684255eb0d19f8299a02a62bae9965d0b57b
l025-l <cristianpegatodo22@gmail.com>
2026-09-17 19:14:42 -0500

    posicion aleatoria del limon

--- limones.js
+++ limones.js
@@ -10,13 +10,13 @@ const ALTO_LIMON=20;
 let personajeX=canvas.width/2;
 let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX=canvas.width/2;
-let limonY=0;
+let limonY=5;
 
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
-    dibujarLimon();
+    aparecerLimon();
 }
 
 function dibujarSuelo(){
@@ -32,13 +32,13 @@ function dibujarPersonaje(){
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
-    dectectarColision();
+    
 }
 
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
-    dectectarColision();
+    
 }
 
 function actualizarPantalla(){
@@ -60,6 +60,7 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY= limonY + 10;
     actualizarPantalla();
+    dectectarColision();
 }
 
 function dectectarColision(){
@@ -67,6 +68,18 @@ function dectectarColision(){
         limonX < personajeX+ANCHO_PERSONAJE &&
         limonY+ALTO_LIMON>personajeY &&
         limonY < personajeY+ALTURA_PERSONAJE){
-        alert("ATRAPADO!!!");
+        //alert("ATRAPADO!!!");
+        aparecerLimon();
     }
+}
+
+function probarAleatorio(){
+    let aleatorio=generarAleatorio(10,80);
+    console.log(aleatorio);
+}
+
+function aparecerLimon(){
+    limonX=generarAleatorio(onabort,canvas.width-ANCHO_LIMON);
+    limonY=0
+    actualizarPantalla();
 }
---
+++ utils.js
@@ -0,0 +1,9 @@
+function generarAleatorio(min,max){
+    let random=Math.random();  //0-1
+    //ejemplo : max es 600, min es 5
+    let numero=random*(max-min);  //0-max
+    let numeroEntero=parseInt(numero);
+    //ejemplo: 0
+    numeroEntero=numeroEntero+min;  //5-600
+    return numero;
+}
