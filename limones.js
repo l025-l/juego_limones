5cf8b3e3fa3ed3fed36ec0f2f078e0c2b1360731
l025-l <cristianpegatodo22@gmail.com>
2026-09-21 19:22:06 -0500

    SetInterval

--- limones.js
+++ limones.js
@@ -13,9 +13,11 @@ let limonX=canvas.width/2;
 let limonY=5;
 let puntaje=0;
 let vidas=3
+let velocidadCaida=100;
 
 
 function iniciar(){
+    setInterval(bajarLimon,velocidadCaida); //primerParametro : funcion segundoParametro: tiempo de ms
     dibujarSuelo();
     dibujarPersonaje();
     aparecerLimon();
